b862ac22165b608a22c12bcfddbe99e70f733579
Christian Abbott <christian@dhcp-200-55.eduroam.carleton.ca>
2017-11-28 19:25:27 -0500

    Added lmdb reader

--- tools/utils.py
+++ tools/utils.py
@@ -32,6 +32,32 @@ def load_pascal():
     X = _load_X()
     pass
 
+def read_images_from_lmdb(db_name, visualize):
+    env = lmdb.open(db_name)
+    txn = env.begin()
+    cursor = txn.cursor()
+    X = []
+    y = []
+    idxs = []
+    for idx, (key, value) in enumerate(cursor):
+        datum = caffe_pb2.Datum()
+        datum.ParseFromString(value)
+        X.append(np.array(datum_to_array(datum)))
+        y.append(datum.label)
+        idxs.append(idx)
+    if visualize:
+        print "Visualizing a few images..."
+        for i in range(9):
+            img = X[i]
+            plt.subplot(3,3,i+1)
+            plt.imshow(img)
+            plt.title(y[i])
+            plt.axis('off')
+        plt.show()
+    print " ".join(["Reading from", db_name, "done!"])
+    return X, y, idxs
+
+
 def main():
     load_pascal()
 
