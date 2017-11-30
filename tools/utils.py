855bd8ef5074c842ad23a7f2d8dc8f603a63fbf9
chrisabbott <christianabbott66@gmail.com>
2017-11-30 01:05:03 -0500

    Setup multithreaded training + QueueRunner

--- tools/utils.py
+++ tools/utils.py
@@ -1,85 +1,75 @@
 import os
-#import cv2
+import lmdb
 import numpy as np
+import tensorflow as tf
+import protobuf.datum_pb2
 
+PATH = "/home/christian/Data/ILSVRC15/processed/ilsvrc12_val_lmdb/"
+
+''' Generate a list of all keys in lmdb environment
+        Args: lmdb
+        Returns: List of keys in lmdb environment
 '''
-def _load_X():
-    label_dir = 'VOCdevkit/VOC2012/labels/'
-    image_dir = 'VOCdevkit/VOC2012/JPEGImages/'
-    X = []
-
-    if not os.path.exists(label_dir):
-        print("Generate labels before loading dataset.")
-        return
-
-    if not os.path.exists(image_dir):
-        print("No image directory found.")
-        return
-
-    for i in os.listdir(label_dir):
-        path = "%s%s.jpg" % (image_dir,
-                             os.path.splitext(i)[0])
-        X.append(cv2.imread(path, 1))
-
-    return X
-'''
-
-def _format_y():
-    pass
-
-def _load_y():
-    pass
-
-def load_pascal():
-    X = _load_X()
-    pass
-
-def read_images_from_lmdb(db_name, visualize):
-    env = lmdb.open(db_name)
-    txn = env.begin()
+def get_keys(env, n=0):
+  keys = []
+  
+  with env.begin() as txn:
     cursor = txn.cursor()
-    X = []
-    y = []
-    idxs = []
-    for idx, (key, value) in enumerate(cursor):
-        datum = caffe_pb2.Datum()
-        datum.ParseFromString(value)
-        X.append(np.array(datum_to_array(datum)))
-        y.append(datum.label)
-        idxs.append(idx)
-    if visualize:
-        print("Visualizing a few images...")
-        for i in range(9):
-            img = X[i]
-            plt.subplot(3,3,i+1)
-            plt.imshow(img)
-            plt.title(y[i])
-            plt.axis('off')
-        plt.show()
-    return X, y, idxs
 
-def imagenet_pipeline(env, keys, epochs=90, batch_size=256):
+    i = 0
+
+    for key, value in cursor:
+      if i >= n and n > 0:
+        break;
+
+      keys.append(key)
+
+  return keys
+
+
+''' Decode an entry in lmdb environment by key
+        Args: lmdb, key to decode
+        Returns: decoded entry in lmdb
+'''
+def _decode(env, key):
+  image = False
+  label = False
+
+  with env.begin() as txn:
+    raw = txn.get(key)
+    datum = datum_pb2.Datum()
+    datum.ParseFromString(raw)
+
+    label = datum.label
+
+    if datum.data:
+      image = np.fromstring(datum.data, dtype=np.uint8).reshape(datum.channels, datum.height, datum.width).transpose(1,2,0)
+    else:
+      image = np.array(datum.float_data).astype(np.float).reshape(datum.channels, datum.height, datum.width).transpose(1,2,0)
+
+  return image, label, key
+
+
+''' Specify a training data pipeline that randomly generates batches of tensors
+        Args: lmdb, list of keys, epochs, batch size
+        Returns: List of tensors
+'''
+def data_pipeline(env, keys, epochs=90, batch_size=256):
     # Create a queue of keys
-    producer = tf.train.string_input_producer(keys, num_epochs=epochs)
-    key = producer.dequeue()
+  producer = tf.train.string_input_producer(keys, num_epochs=epochs, shuffle=True)
+  key = producer.dequeue()
 
-    def retrieve_batch(key):
-        with env.begin() as txn:
-            val = txn.get(key)
+  def retrieve_batch(key):
+      with env.begin() as txn:
+        X_s, y_s, key = _decode(env, key)
 
-        X_s = get_example_from_val(val)
-        y_s = get_label_from_val(val)
+      return X_s, y_s
 
-        return X_s, y_s
+  # Single X and y value pair
+  X_s, y_s = tf.py_func(retrieve_batch, [key], [tf.float64, tf.float64])
+  X_s.set_shape([224,224,3])
+  y_s.set_shape([1])
+  
+  X, y = tf.train.batch([X_s, y_s], batch_size)
 
-    # Single X and y value pair
-    X_s, y_s = tf.py_func(retrieve_batch, [key], [tf.float64, tf.float64])
-    X, y = tf.train.batch([X_s, y_s], batch_size)
-
-    return X, y
-
-def main():
-    load_pascal()
-
-if __name__ == '__main__':
-    main()
+  return X, y
