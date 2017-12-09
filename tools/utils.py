294ec9d3ca9e2c9fdfac7f7758c5a9c83fa86b36
Christian Abbott <christian@Christians-MacBook-Pro.local>
2017-12-09 01:55:03 -0500

    Restructure in progress

--- tools/utils.py
+++ tools/utils.py
@@ -1,75 +1,32 @@
-import os
-import lmdb
-import numpy as np
 import tensorflow as tf
-from tools.protobuf import datum_pb2
-
-PATH = "/home/christian/Data/ILSVRC15/processed/ilsvrc12_val_lmdb/"
-
-''' Generate a list of all keys in lmdb environment
-        Args: lmdb
-        Returns: List of keys in lmdb environment
-'''
-def get_keys(env, n=0):
-  keys = []
-  
-  with env.begin() as txn:
-    cursor = txn.cursor()
-
-    i = 0
-
-    for key, value in cursor:
-      if i >= n and n > 0:
-        break;
-
-      keys.append(key)
-
-  return keys
+import os
 
 
-''' Decode an entry in lmdb environment by key
-        Args: lmdb, key to decode
-        Returns: decoded entry in lmdb
-'''
-def _decode(env, key):
-  image = False
-  label = False
+_FILE_PATTERN = 'train-%s-of-%s.tfrecord'
+_SPLITS_TO_SIZES = {'train': 1281167, 'test': 50000}
+_NUM_CLASSES = 1000
+_ITEMS_TO_DESCRIPTIONS = {
+    'image': 'A [224 x 224 x 3] RGB image.',
+    'label': 'A single integer between 0 and 999',
+}
 
-  with env.begin() as txn:
-    raw = txn.get(key)
-    datum = datum_pb2.Datum()
-    datum.ParseFromString(raw)
+def preprocess(image, output_height, output_width, is_training):
+  image = tf.to_float(image)
+  image = tf.image.resize_image_with_crop_or_pad(
+    image, output_width, output_height)
+  image = tf.subtract(image, 128.0)
+  image = tf.div(image, 128.0)
+  return image
 
-    label = datum.label
+def load_batch(dataset, batch_size=32, height=224, width=224, is_training=False):
+  data_provider = slim.dataset_data_provider.DatasetDataProvider(dataset)
 
-    if datum.data:
-      image = np.fromstring(datum.data, dtype=np.uint8).reshape(datum.channels, datum.height, datum.width).transpose(1,2,0)
-    else:
-      image = np.array(datum.float_data).astype(np.float).reshape(datum.channels, datum.height, datum.width).transpose(1,2,0)
+  image, label = data_provider.get(['image', 'label'])
+  image = preprocess(image, height, width, is_training)
 
-  return image, label, key
+  images, labels = tf.train.batch(
+    [image, label],
+    batch_size=batch_size,
+    allow_smaller_final_batch=True)
 
-
-''' Specify a training data pipeline that randomly generates batches of tensors
-        Args: lmdb, list of keys, epochs, batch size
-        Returns: List of tensors
-'''
-def data_pipeline(env, keys, epochs=90, batch_size=256):
-    # Create a queue of keys
-  producer = tf.train.string_input_producer(keys, num_epochs=epochs, shuffle=True)
-  key = producer.dequeue()
-
-  def retrieve_batch(key):
-      with env.begin() as txn:
-        X_s, y_s, key = _decode(env, key)
-
-      return X_s, y_s
-
-  # Single X and y value pair
-  X_s, y_s = tf.py_func(retrieve_batch, [key], [tf.float64, tf.float64])
-  X_s.set_shape([224,224,3])
-  y_s.set_shape([1])
-  
-  X, y = tf.train.batch([X_s, y_s], batch_size)
-
-  return X, y
+  return images, labels
