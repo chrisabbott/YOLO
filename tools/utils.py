82bf5316209dafa6618d26a6f8397f9a201a9feb
Christian Abbott <christian@Christians-MacBook-Pro.local>
2017-12-11 18:46:08 -0500

    Integrating training loop

--- tools/utils.py
+++ tools/utils.py
@@ -9,40 +9,10 @@ import tensorflow as tf
 FLAGS = tf.app.flags.FLAGS
 slim = tf.contrib.slim
 
-# Define os and dataset flags
-tf.app.flags.DEFINE_string('data_dir', '/home/christian/Data/ILSVRC/tfrecords/', 'Path to data directory')
-tf.app.flags.DEFINE_string('train_dir', '/home/christian/Data/ILSVRC/tfrecords/train-*', 'Path to training data')
-tf.app.flags.DEFINE_string('val_dir', '/home/christian/Data/ILSVRC/tfrecords/validation-*', 'Path to validation data')
-tf.app.flags.DEFINE_string('train_data_pattern', 'train-%d-of-01024', 'Training tfrecord filename')
-tf.app.flags.DEFINE_string('val_data_pattern', 'validation-%d-of-00128', 'Validation tfrecord filename')
-tf.app.flags.DEFINE_integer('train_samples', 1281167, 'Number of training samples in ImageNet')
-tf.app.flags.DEFINE_integer('validation_samples', 50000, 'Number of validation samples in ImageNet')
-tf.app.flags.DEFINE_integer('num_classes', 1001, 'Number of classes in ImageNet, plus one for Null class')
-tf.app.flags.DEFINE_integer('input_queue_memory_factor', 16,
-                            """Size of the queue of preprocessed images. """
-                            """Default is ideal but try smaller values, e.g. """
-                            """4, 2 or 1, if host memory is constrained. See """
-                            """comments in code for more details.""")
-
-# Define training flags
-tf.app.flags.DEFINE_float('initial_learning_rate', 0.1, 'Initial learning rate')
-tf.app.flags.DEFINE_float('num_epochs_per_decay', 30.0, 'Number of epochs per decay')
-tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.16, 'Learning rate decay factor')
-tf.app.flags.DEFINE_integer('batch_size', 32, 'Batch size')
-tf.app.flags.DEFINE_integer('image_size', 299, 'Image size')
+# Define os flags
 tf.app.flags.DEFINE_integer('num_threads', 4, 'Number of threads to use in preprocessing and loading')
 tf.app.flags.DEFINE_integer('num_readers', 4, 'Number of readers to use in loading')
-tf.app.flags.DEFINE_integer('max_steps', 100000, 'Maximum number of steps before termination')
-tf.app.flags.DEFINE_integer('num_epochs', 300, 'Total number of epochs')
-
-# Define learning rate schedule
-RMSPROP_DECAY = 0.9
-RMSPROP_MOMENTUM = 0.9
-RMSPROP_EPSILON = 1.0
-
-# Define a list of data files
-TRAIN_SHARDS = tf.gfile.Glob(FLAGS.train_dir)
-VAL_SHARDS = tf.gfile.Glob(FLAGS.val_dir)
+tf.app.flags.DEFINE_integer('input_queue_memory_factor', 16, 'Hyperparameter for memory usage')
 
 
 def decode_jpeg(image_buffer, scope=None):
@@ -262,7 +232,7 @@ def image_preprocessing(image_buffer, bbox, train, thread_id=0):
   image = tf.multiply(image, 2.0)
   return image
 
-def load_batch(shards=TRAIN_SHARDS, train=True, batch_size=FLAGS.batch_size, num_epochs=FLAGS.num_epochs):
+def load_batch(batch_size, num_epochs, shards, train=True):
   with tf.name_scope('load_batch'):
     if train:
       filename_queue = tf.train.string_input_producer(shards, shuffle=True, capacity=16)
--- train.py
+++ train.py
@@ -4,9 +4,29 @@ import numpy as np
 import tensorflow as tf
 
 from tools import utils
+from models.slim import model
+import tensorflow.contrib.slim.nets as nets
+vgg = nets.vgg
 
 FLAGS = tf.app.flags.FLAGS
 slim = tf.contrib.slim
+
+# Define os and dataset flags
+tf.app.flags.DEFINE_string('data_dir', '/Users/christian/Data/ILSVRC/tfrecords/', 'Path to data directory')
+tf.app.flags.DEFINE_string('train_dir', '/Users/christian/Data/ILSVRC/tfrecords/train-*', 'Path to training data')
+tf.app.flags.DEFINE_string('val_dir', '/Users/christian/Data/ILSVRC/tfrecords/validation-*', 'Path to validation data')
+tf.app.flags.DEFINE_integer('train_samples', 1281167, 'Number of training samples in ImageNet')
+tf.app.flags.DEFINE_integer('validation_samples', 50000, 'Number of validation samples in ImageNet')
+tf.app.flags.DEFINE_integer('num_classes', 1000, 'Number of classes in ImageNet, plus one for Null class')
+
+# Define training flags
+tf.app.flags.DEFINE_float('initial_learning_rate', 0.1, 'Initial learning rate')
+tf.app.flags.DEFINE_float('num_epochs_per_decay', 30.0, 'Number of epochs per decay')
+tf.app.flags.DEFINE_float('learning_rate_decay_factor', 0.16, 'Learning rate decay factor')
+tf.app.flags.DEFINE_integer('batch_size', 32, 'Batch size')
+tf.app.flags.DEFINE_integer('image_size', 299, 'Image size')
+tf.app.flags.DEFINE_integer('max_steps', 100000, 'Maximum number of steps before termination')
+tf.app.flags.DEFINE_integer('num_epochs', 300, 'Total number of epochs')
 
 # Define learning rate schedule
 RMSPROP_DECAY = 0.9
@@ -18,4 +38,30 @@ TRAIN_SHARDS = tf.gfile.Glob(FLAGS.train_dir)
 VAL_SHARDS = tf.gfile.Glob(FLAGS.val_dir)
 
 def train():
-  pass
+  with tf.Graph().as_default():
+    X = tf.placeholder(tf.float32,
+                       shape=(FLAGS.batch_size,
+                              FLAGS.image_size,
+                              FLAGS.image_size,
+                              3))
+
+    #net = model.tiny_yolo(X, pretrain=True)
+
+    images, labels = utils.load_batch(batch_size=FLAGS.batch_size, 
+                                    num_epochs=FLAGS.num_epochs, 
+                                    shards=TRAIN_SHARDS)
+
+    predictions = model.tiny_yolo(X, pretrain=True)
+    #predictions = vgg.vgg_16(images, is_training=True)
+
+    #predictions = net(images)
+    loss = slim.losses.softmax_cross_entropy(predictions, labels)
+
+    total_loss = slim.losses.get_total_loss()
+    tf.summry_scalar('losses/total_loss', total_loss)
+
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_tensor = slim.learning.create_train_op(total_loss, optimizer)
+    slim.learning.train(train_tensor, train_log_dir)
+
+train()
